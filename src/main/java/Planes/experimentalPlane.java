7402876f26767dbcd80539a5345f180308cdc843
Alexandr Lobashov <viper.489@inbox.ru>
2020-09-26 00:08:06 +0300

    Cleaned up ExperimentalPlane.java

--- src/main/java/Planes/experimentalPlane.java
+++ src/main/java/Planes/experimentalPlane.java
@@ -3,12 +3,12 @@ package Planes;
 import models.ClassificationLevel;
 import models.ExperimentalTypes;
 
-public class experimentalPlane extends Plane{
+public class ExperimentalPlane extends Plane{
 
-    private ExperimentalTypes type;
+    private final ExperimentalTypes type;
     private ClassificationLevel classificationLevel;
 
-    public experimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes type, ClassificationLevel classificationLevel) {
+    public ExperimentalPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity, ExperimentalTypes type, ClassificationLevel classificationLevel) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
         this.type = type;
         this.classificationLevel = classificationLevel;
@@ -20,6 +20,10 @@ public class experimentalPlane extends Plane{
 
     public void setClassificationLevel(ClassificationLevel classificationLevel){
         this.classificationLevel = classificationLevel;
+    }
+
+    public ExperimentalTypes getType() {
+        return type;
     }
 
     @Override
